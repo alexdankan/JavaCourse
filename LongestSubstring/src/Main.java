bd304dde6a7076789eaf59386809daf1ccb76b95
Alex <you@example.com>
2019-01-29 19:37:48 +0100

    added comment 2

--- LongestSubstring/src/Main.java
+++ LongestSubstring/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		
 		System.out.println(geLongestSubstring(input));
 	}
-//solution 3
+//solution 3 test
 	public static int geLongestSubstring(String input) {
 		
 		int count=1;
